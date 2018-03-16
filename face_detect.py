34257995b47ce1e8085752f2bbc6e2580524a2dd
Dengke Du <dengke.du@windriver.com>
2018-03-16 14:45:22 +0800

    save the result
    Signed-off-by: Dengke Du <dengke.du@windriver.com>

--- face_detect.py
+++ face_detect.py
@@ -16,5 +16,6 @@ for (x,y,w,h) in faces:
     for (ex,ey,ew,eh) in eyes:
         cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 cv2.imshow('img',img)
+cv2.imwrite('../result.jpg',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
