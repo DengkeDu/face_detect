37f944d8f3542548180862083bac7bd3e072e558
Dengke Du <dengke.du@windriver.com>
2018-05-15 18:10:44 +0800

    Using the rtsp server as the input
    Signed-off-by: Dengke Du <dengke.du@windriver.com>

--- face_detect.py
+++ face_detect.py
@@ -4,18 +4,23 @@ import cv2
 face_cascade = cv2.CascadeClassifier('/home/wrsadmin/opencv/opencv-3.4.0/data/haarcascades/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('/home/wrsadmin/opencv/opencv-3.4.0/data/haarcascades/haarcascade_eye.xml')
 
-img = cv2.imread('du.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cap = cv2.VideoCapture("rtspsrc location=rtsp://128.224.176.192:8554/test latency=0 ! rtph264depay ! decodebin ! videoconvert ! appsink")
 
-faces = face_cascade.detectMultiScale(gray, 1.1, 5)
-for (x,y,w,h) in faces:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-cv2.imshow('img',img)
-cv2.imwrite('../result.jpg',img)
-cv2.waitKey(0)
+while True:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    cv2.imshow('img',img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
 cv2.destroyAllWindows()
